414ad8fc6df8bdab8be4d7175b4539ef036d06c2
grenwick <grenwick@mines.edu>
2024-01-29 13:37:01 -0700

    changed bullet holes to not appear on enemies

--- pickups/guns/gun.gd
+++ pickups/guns/gun.gd
@@ -23,14 +23,15 @@ func shoot(shot_spread, shot_trails, gun_name, gun_barrel):
 		var bullet_array = [bullethole.instantiate(), bullethole.instantiate(), bullethole.instantiate(), bullethole.instantiate(), bullethole.instantiate(), bullethole.instantiate(), bullethole.instantiate()]
 		var increment = 0
 		for i in bullet_array:
-			i.set_as_top_level(true)
-			get_parent().add_child(i)
 			
 			#make bullet trail, either it collides with object, or shoot it into space
 			instance = bullet_trail.instantiate()
 			if shot_spread[increment].is_colliding():
 				if shot_spread[increment].get_collider().is_in_group("enemy"):
 					shot_spread[increment].get_collider().hit(20)
+				else:
+					i.set_as_top_level(true)
+					get_parent().add_child(i)
 				instance.init(gun_barrel.global_position, shot_spread[increment].get_collision_point())
 			else:
 				instance.init(gun_barrel.global_position, shot_trails[increment].global_position)
@@ -40,34 +41,36 @@ func shoot(shot_spread, shot_trails, gun_name, gun_barrel):
 			increment += 1
 			
 	if gun_name == "Lyre":
-		var bulletInst = bullethole.instantiate() as Node3D
-		bulletInst.set_as_top_level(true)
-		get_parent().add_child(bulletInst)
-		
-		#make bullet trail, either it collides with object, or shoot it into space
-		instance = bullet_trail.instantiate()
-		if shot_spread[0].is_colliding():
-			if shot_spread[0].get_collider().is_in_group("enemy"):
-					shot_spread[0].get_collider().hit(20)
-			instance.init(gun_barrel.global_position, shot_spread[0].get_collision_point())
-		else:
-			instance.init(gun_barrel.global_position, shot_trails[0].global_position)
-		get_parent().add_child(instance)
-		bulletInst.global_transform.origin = shot_spread[0].get_collision_point() as Vector3
-		
-	if gun_name == "Revolver":
-		var bulletInst = bullethole.instantiate() as Node3D
-		bulletInst.set_as_top_level(true)
-		get_parent().add_child(bulletInst)
-		
 		#make bullet trail, either it collides with object, or shoot it into space
 		instance = bullet_trail.instantiate()
 		if shot_spread[0].is_colliding():
 			if shot_spread[0].get_collider().is_in_group("enemy"):
 				shot_spread[0].get_collider().hit(20)
+			else:
+				var bulletInst = bullethole.instantiate() as Node3D
+				bulletInst.set_as_top_level(true)
+				get_parent().add_child(bulletInst)
+				bulletInst.global_transform.origin = shot_spread[0].get_collision_point() as Vector3
 			instance.init(gun_barrel.global_position, shot_spread[0].get_collision_point())
 		else:
 			instance.init(gun_barrel.global_position, shot_trails[0].global_position)
 		get_parent().add_child(instance)
-		bulletInst.global_transform.origin = shot_spread[0].get_collision_point() as Vector3
+		
+		
+	if gun_name == "Revolver":
+		#make bullet trail, either it collides with object, or shoot it into space
+		instance = bullet_trail.instantiate()
+		if shot_spread[0].is_colliding():
+			if shot_spread[0].get_collider().is_in_group("enemy"):
+				shot_spread[0].get_collider().hit(20)
+			else:
+				var bulletInst = bullethole.instantiate() as Node3D
+				bulletInst.set_as_top_level(true)
+				get_parent().add_child(bulletInst)
+				bulletInst.global_transform.origin = shot_spread[0].get_collision_point() as Vector3
+			instance.init(gun_barrel.global_position, shot_spread[0].get_collision_point())
+		else:
+			instance.init(gun_barrel.global_position, shot_trails[0].global_position)
+		get_parent().add_child(instance)
+		
 	
