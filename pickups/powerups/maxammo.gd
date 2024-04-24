61823b88258b1a2b1e60d1f8c83e71d77ac01a23
Cade Forbes <clforbes@mines.edu>
2024-04-24 09:24:27 -0600

    max ammo working

---
+++ pickups/powerups/maxammo.gd
@@ -0,0 +1,21 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	##if(player position is within collision shape) {destroy this instance, refill player ammo and reserves}
+	#var player_position = Player.global_position #grab player position
+	#var collision_shape = $CollisionShape3D #grab the max ammo's collision shape
+	##compare them
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):
+		GlobalVars.held_object.current_reserves = GlobalVars.held_object.reserves_size
+		queue_free();
